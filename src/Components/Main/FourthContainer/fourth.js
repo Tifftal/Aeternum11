6280a7955e14a7ac8633699da80272a3f3b8e09c
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-08-16 23:51:09 +0900

    v 0.6 - Newsletter functionality

--- src/Components/Main/FourthContainer/fourth.js
+++ src/Components/Main/FourthContainer/fourth.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./fourth.css";
 import { useTranslation } from "react-i18next";
 
 const Fourth = () => {
     const [t] = useTranslation("global");
+
+    const [email, setEmail] = useState("");
+    const [selectedGender, setSelectedGender] = useState("");
+    const [agreed, setAgreed] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [genderError, setGenderError] = useState(false);
+    const [agreedError, setAgreedError] = useState(false);
+
+    const handleEmailChange = (e) => {
+        const value = e.target.value;
+        setEmail(value);
+        setEmailError(value === "");
+    }
+
+    const handleGenderChange = (e) => {
+        setSelectedGender(e.target.value);
+        setGenderError(false);
+    }
+
+    const handleAgreeChange = (e) => {
+        setAgreed(e.target.checked);
+        setAgreedError(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (email === "") {
+            setEmailError(true);
+        }
+
+        if (!selectedGender) {
+            setGenderError(true);
+        }
+
+        if (!agreed) {
+            setAgreedError(true);
+        }
+
+        console.log("Form submitted successfully!");
+    }
+
     return (
         <div className="fourth">
             <div className="fourthInfo">
@@ -12,32 +54,65 @@ const Fourth = () => {
             <div className="NewSt">
                 <h1>{t("fourth.title")}</h1>
                 <p>{t("fourth.text")}</p>
-                <div className="emailForm">
-                    <input className="email" type="email" id="email" name="email"/>
+                <form
+                    className="emailForm"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        className={emailError ? "email error" : "email"}
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={handleEmailChange}
+                    />
                     <label for="email" className="email-label">{t("fourth.email")}</label>
-                    <p>{t("fourth.please")}</p>
+                    {emailError && <p>{t("fourth.emailError")}</p>}
                     <div className="chooseGender">
                         <div className="gender" >
-                            <input type="checkbox" />
+                            <input
+                                type="checkbox"
+                                name="gender"
+                                value="women"
+                                checked={selectedGender === "women"}
+                                onChange={handleGenderChange}
+                            />
                             <label>{t("fourth.women")}</label>
                         </div>
                         <div className="gender" >
-                            <input type="checkbox" />
+                            <input
+                                type="checkbox"
+                                name="gender"
+                                value="men"
+                                checked={selectedGender === "men"}
+                                onChange={handleGenderChange}
+                            />
                             <label>{t("fourth.men")}</label>
                         </div>
                         <div className="gender" >
-                            <input type="checkbox" />
+                            <input
+                                type="checkbox"
+                                name="gender"
+                                value="none-binary"
+                                checked={selectedGender === "none-binary"}
+                                onChange={handleGenderChange}
+                            />
                             <label>{t("fourth.none-binary")}</label>
                         </div>
                     </div>
+                    {genderError && <p>{t("fourth.genderError")}</p>}
                     <div className="agree">
-                        <input type="checkbox" />
+                        <input
+                            type="checkbox"
+                            checked={agreed}
+                            onChange={handleAgreeChange}
+                        />
                         <label>{t("fourth.agree")}</label>
                     </div>
-                    <a className="BtnSb" href="/"><button>{t("fourth.subscribe")}</button></a>
-
-                    
-                </div>
+                    {agreedError && <p>{t("fourth.agreeError")}</p>}
+                    <span className="BtnSb">
+                        <button type="submit">{t("fourth.subscribe")}</button>
+                    </span>
+                </form>
             </div>
         </div>
     )
