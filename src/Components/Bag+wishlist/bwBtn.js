5adc9518b090af847b741437772a81eb19f523ee
VarTy <talankina.v@mail.ru>
2023-08-18 19:11:35 +0400

    v 0.12

---
+++ src/Components/Bag+wishlist/bwBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './bwBtn.css';
+
+const bwBtn = () => {
+    return (
+        <div className="bwBtn">
+            bwBtn
+        </div>
+    )
+}
+
+export default bwBtn;
