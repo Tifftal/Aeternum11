bb1b80dfc10745018f57a5502f88f7ecefb43d67
VarTy <talankina.v@mail.ru>
2023-08-18 18:53:38 +0400

    v 0.11 account's pages

---
+++ src/Components/Account/PersInf/personal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './personal.css';
+import { useTranslation } from "react-i18next";
+
+const Personal = () => {
+    const [t] = useTranslation("global");
+    return (
+        <div className="pers">
+            <h2>{t("create_account.title1")}</h2>
+            <h1>Join Aeternum Eleven</h1>
+
+            <div className="pers-data">
+                <input type="text" placeholder={t("create_account.first_name")} />
+                <input type="text" placeholder={t("create_account.last_name")} />
+                <input type="text" placeholder={t("create_account.gender")} />
+                <input type="text" placeholder={t("create_account.date")} />
+                <input type="text" placeholder={t("create_account.country")} />
+                <input type="text" placeholder={t("create_account.state")} />
+                <input type="text" placeholder={t("create_account.sity")} />
+                <input type="text" placeholder={t("create_account.phone")} />
+            </div>            
+            <h3>{t("create_account.*")}</h3>
+            <h4>{t("create_account.text2")}</h4>
+            <div className="agreeAc">
+                <input type="checkbox" />
+                <label>{t("create_account.check1")}</label>
+            </div>
+            <div className="agreeAc">
+                <input type="checkbox" />
+                <label>{t("create_account.check2")}</label>
+            </div>
+
+            <a className="BtnAc" href="/"><button>Update your Information</button></a>
+        </div>
+
+    )
+}
+
+export default Personal;
--- src/Components/Bag/bag.js
+++ src/Components/Bag/bag.js
@@ -5,16 +5,9 @@ import { useTranslation } from "react-i18next";
 const Bag = () => {
     const [t] = useTranslation("global");
 
-    const [clas, setClas] = useState('def');
-
-    const HandleClick = (e) => {
-        const newClas = clas === "def" ? "act" : "def";
-        setClas(newClas)
-    };
-
     return (
         <div className="bag">
-            <button className={clas} onClick={HandleClick}>{clas}</button>
+            bag
         </div>
     )
 }
