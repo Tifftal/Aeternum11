dfe59d056211b4318b6bc302be87c9c2920925bb
VarTy <talankina.v@mail.ru>
2023-11-29 01:47:29 +0300

    admin

--- src/Components/Admin/Goods/Goods.js
+++ src/Components/Admin/Goods/Goods.js
@@ -9,6 +9,11 @@ import PopupAddGood from "./Popup/PopupAddGood";
 const Goods = () => {
     const [t] = useTranslation("global");
     const [data, setData] = useState([]);
+    const [free, setFree] = useState([]);
+    const [categories, setCategories] = useState({
+        id: 0,
+        name: ""
+    });
     const [selectedGood, setSelectedGood] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
     const [isOpenAddPopup, setIsOpenAddPopup] = useState(false);
@@ -22,15 +27,66 @@ const Goods = () => {
         setIsOpen(false);
     };
 
-    const HandleOpenAddPopup = () => {
-        setIsOpenAddPopup(true);
-    };
+    // const HandleOpenAddPopup = () => {
+    //     setIsOpenAddPopup(true);
+    // };
 
-    const HandleCloseAddPopup = () => {
-        setIsOpenAddPopup(false);
-    };
+    // const HandleCloseAddPopup = () => {
+    //     setIsOpenAddPopup(false);
+    // };
 
     useEffect(() => {
+        api.get(`${URI}/sections`)
+            .then(response => {
+                const categories = response.data.flatMap(section => section.categories.map(category => category.id));
+                const catArray = response.data.flatMap(section => section.categories.map(category => ({
+                    id: category.id,
+                    name: category.name
+                })));
+                setCategories(catArray);
+                const requests = categories.map(category => (
+                    api.get(`${URI}/category/${category}/goods`)
+                        .then(response => ({
+                            categoryName: response.data.name,
+                            goodsByCategory: response.data.goods.content,
+                        }))
+                        .catch(err => {
+                            console.error(err);
+                            return null;
+                        })
+                ));
+
+                Promise.all(requests)
+                    .then(categoryGoodsArray => {
+                        const validCategoryGoods = categoryGoodsArray.filter(categoryGoods => categoryGoods !== null);
+                        setData(validCategoryGoods);
+
+                        // Обновляем значение categories
+                        const updatedCategories = response.data.flatMap(section => section.categories.map(category => ({
+                            id: category.id,
+                            name: category.name
+                        })));
+                        setCategories(updatedCategories);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        api.get(`${URI}/goods/free`)
+            .then(response => {
+                // console.log(response.data.content);
+                setFree(response.data.content)
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, [data]);
+
+    const UpdateData = () => {
         api.get(`${URI}/sections`)
             .then(response => {
                 const categories = response.data.flatMap(section => section.categories.map(category => category.id));
@@ -59,7 +115,95 @@ const Goods = () => {
             .catch(err => {
                 console.error(err);
             });
-    }, []);
+    };
+
+    const handleDeleteGood = (id) => {
+        api.delete(`${URI}/good/${id}`)
+            .then(response => {
+                console.log(response);
+                // После успешного удаления, вызываем функцию UpdateData
+                UpdateData();
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    const handleCategoryChange = (e, good_id) => {
+        const selectedCategoryId = e.target.value;
+        const selectedCategory = categories.find(category => category.id === parseInt(selectedCategoryId));
+
+        if (selectedCategory) {
+            console.log("Selected Category ID:", selectedCategory.id);
+            console.log("Selected Category Name:", selectedCategory.name);
+            console.log("Selected Good ID:", good_id);
+
+            api.put(`${URI}/good/${good_id}/categories`,
+                [
+                    {
+                        id: selectedCategoryId
+                    }
+                ],
+                {
+                    headers: {
+                        Authorization: `Bearer ${window.localStorage.getItem("jwtToken")}`,
+                    },
+                }
+            )
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        }
+        UpdateData();
+    };
+
+    const AddGood = () => {
+        api.post(`${URI}/good`,
+            {
+                name: 'Название',
+                description: 'Описание',
+                compound: 'Состав',
+                cost: 0,
+                backColor: {
+                    name: 'Чёрный',
+                    code: 'black',
+                },
+                colors: [
+                    {
+                        name: 'Чёрный',
+                        code: 'black',
+                        sizes: [
+                            {
+                                size: 'Размер',
+                                sizeStatus: 'IN_STOCK',
+                            },
+                        ],
+                    },
+                ],
+                sizes: [
+                    {
+                        size: 'Размер',
+                        sizeStatus: 'IN_STOCK',
+                    },
+                ],
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${window.localStorage.getItem("jwtToken")}`,
+                },
+            }
+        )
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+            UpdateData();
+    }
 
     return (
         <div className="goodsAdmin">
@@ -101,38 +245,76 @@ const Goods = () => {
                     }}
                 />
             )}
-            {isOpenAddPopup && (
-                <PopupAddGood
-                    onClose={() => HandleCloseAddPopup()} 
-                    setIsOpen={setIsOpenAddPopup}
-                />
-            )}
 
             <div className="AddGoodAdmin">
-                <button className="AddBtnAdmin" onClick={() => HandleOpenAddPopup()}>Добавить товар</button>
+                <button className="AddBtnAdmin" onClick={AddGood}>Добавить товар</button>
             </div>
+
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th colSpan="8" style={{ backgroundColor: "rgba(217, 217, 217, 0.2)", textAlign: "center", fontSize: "100%" }}>Товары без категории</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th>Название</th>
+                        <th>Цена</th>
+                        <th>Статус</th>
+                        <th>Цвета</th>
+                        <th>Размеры</th>
+                        <th>Категория</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {free.map(good => (
+                        <tr key={good.id}>
+                            <td style={{ width: "5%" }}><button onClick={() => HandleOpenNote(good)} className="edit"><img src="../../IMG/icons8-редактировать-144.png" /></button></td>
+                            <td style={{ width: "30%" }}>{good.name}</td>
+                            <td style={{ width: "5%" }}>{good.cost} ₽</td>
+                            <td style={{ width: "10%" }}>{good.state}</td>
+                            <td style={{ width: "15%" }}><button className="editSize">Изменить цвета</button></td>
+                            <td style={{ width: "15%" }}><button className="editSize">Изменить размеры</button></td>
+                            <td style={{ width: "20%" }}>
+                                <form>
+                                    <select onChange={(e) => handleCategoryChange(e, good.id)}>
+                                        <option disabled selected value="">Выберите категорию</option>
+                                        {categories.map(category => (
+                                            <option key={category.id} value={category.id}>{category.name}</option>
+                                        ))}
+                                    </select>
+                                </form>
+                            </td>
+                            <td style={{ width: "5%" }}><button onClick={() => handleDeleteGood(good.id)} className="edit" style={{ backgroundColor: "#472828" }}><img src="../../IMG/icons8-крестик-78.png" style={{ filter: "invert(1)" }} /></button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
             {data.map(category => (
                 <React.Fragment key={category.categoryName}>
                     <table className="table">
                         <thead>
                             <tr>
-                                <th colSpan="4" style={{ backgroundColor: "rgba(217, 217, 217, 0.2)", textAlign: "center", fontSize: "100%" }}>{category.categoryName}</th>
+                                <th colSpan="5" style={{ backgroundColor: "rgba(217, 217, 217, 0.2)", textAlign: "center", fontSize: "100%" }}>{category.categoryName}</th>
                             </tr>
                             <tr>
                                 <th></th>
                                 <th>Название</th>
-                                <th>Стоимость</th>
+                                <th>Цена</th>
                                 <th>Статус</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             {category.goodsByCategory.map(good => (
                                 <tr key={good.id}>
-                                    <td style={{ width: "5%" }}><button onClick={() => HandleOpenNote(good)}><img src="../../IMG/icons8-редактировать-144.png" /></button></td>
-                                    <td style={{ width: "65%" }}>{good.name}</td>
+                                    <td style={{ width: "5%" }}><button onClick={() => HandleOpenNote(good)} className="edit"><img src="../../IMG/icons8-редактировать-144.png" /></button></td>
+                                    <td style={{ width: "60%" }}>{good.name}</td>
                                     <td style={{ width: "15%" }}>{good.cost} ₽</td>
                                     <td style={{ width: "15%" }}>{good.state}</td>
+                                    <td style={{ width: "5%" }}><button onClick={() => handleDeleteGood(good.id)} style={{ backgroundColor: "#472828" }} className="edit"><img src="../../IMG/icons8-крестик-78.png" style={{ filter: "invert(1)" }} /></button></td>
                                 </tr>
                             ))}
                         </tbody>
--- src/Components/Admin/Goods/Popup/Popup.js
+++ src/Components/Admin/Goods/Popup/Popup.js
@@ -80,60 +80,60 @@ const Popup = ({ onClose, setIsOpen, selectedGood, onDataUpdate }) => {
                 </div>
                 <div className='flex-cont'>
                     <div>Тут фото</div>
-                    <form>
-                        <div className='form-group'>
+                    <form className='form-edit'>
+                        <div className='form-group-edit'>
                             <label htmlFor="name" className='label'>Название:</label>
                             <input
                                 id="name"
-                                className='good-input'
+                                className='good-input-edit'
                                 value={editedGood.name}
                                 onChange={(e) => handleInputChange(e, 'name')}
                             />
                         </div>
 
-                        <div className='form-group'>
+                        <div className='form-group-edit'>
                             <label htmlFor="cost" className='label'>Стоимость:</label>
                             <input
                                 id="cost"
-                                className='good-input'
+                                className='good-input-edit'
                                 value={editedGood.cost}
                                 onChange={(e) => handleInputChange(e, 'cost')}
                             />
                         </div>
 
-                        <div className='form-group'>
+                        <div className='form-group-edit'>
                             <label htmlFor="compound" className='label'>Состав:</label>
                             <input
                                 id="compound"
-                                className='good-input'
+                                className='good-input-edit'
                                 value={editedGood.compound}
                                 onChange={(e) => handleInputChange(e, 'compound')}
                             />
                         </div>
 
-                        <div className='form-group'>
+                        <div className='form-group-edit'>
                             <label htmlFor="description" className='label'>Описание:</label>
                             <textarea
                                 id="description"
-                                className='good-input text-area'
+                                className='good-input-edit text-area'
                                 value={editedGood.description}
                                 onChange={(e) => handleInputChange(e, 'description')}
                             />
                         </div>
 
-                        <div className='form-group'>
+                        <div className='form-group-edit'>
                             <label htmlFor="backColor" className='label'>Цвет/Код фона:</label>
                             <div>
                                 <input
                                     id="backColorName"
-                                    className='good-input'
+                                    className='good-input-edit'
                                     style={{ width: '7.5vw' }}
                                     value={editedGood.backColor.name}
                                     onChange={(e) => handleInputChange(e, 'backColor.name')}
                                 />
                                 <input
                                     id="backColorCode"
-                                    className='good-input'
+                                    className='good-input-edit'
                                     style={{ width: '7.5vw'}}
                                     value={editedGood.backColor.code}
                                     onChange={(e) => handleInputChange(e, 'backColor.code')}
